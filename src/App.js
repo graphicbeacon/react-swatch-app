bf252b83fd3df65346444ea09b65324dab99bb22
Jermaine Oppong <jeropp00@gmail.com>
2017-03-26 20:17:07 +0100

    Implemented working solution. Data flows with lifting state up approach.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import SwatchSelectApp from './components/SwatchSelectApp';
-import SwatchSelectSlider from './components/SwatchSelectSlider';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <SwatchSelectApp>
-        <SwatchSelectSlider />
-        <SwatchSelectSlider />
-        <SwatchSelectSlider />
+        
       </SwatchSelectApp>
       </div>
     );
--- src/components/SwatchSelectSlider.js
+++ src/components/SwatchSelectSlider.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 
 class SwatchSelectSlider extends Component {
+    constructor(props) {
+        super(props);
+        //
+        this.state = {
+            value: 0
+        }
+    }
+
+    update(e) {
+        let targetValue = e.target.value;
+        this.setState({value: targetValue})
+        this.props.onChange({
+            color: this.props.color,
+            value: targetValue
+        });
+    }
+
     render() {
         return (
             <div>
-                <input type="range" />
-                <input type="number" />
+                <input 
+                    type="range" 
+                    min={this.props.min}
+                    max={this.props.max}
+                    step={this.props.step}
+                    value={this.state.value}
+                    onChange={this.update.bind(this)}
+                />
+                <input 
+                    type="number" 
+                    min={this.props.min}
+                    max={this.props.max}
+                    step={this.props.step}
+                    value={this.state.value}
+                    onChange={this.update.bind(this)}
+                />
             </div>
         );
     }

ad0437675c5e2223efa242c06a84c3008f2087ce
Jermaine Oppong <jermaine.oppong@neyber.co.uk>
2017-03-27 16:29:47 +0100

    Refactors SwatchPreview component into stateless function.

--- src/components/SwatchPreview.js
+++ src/components/SwatchPreview.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-class SwatchPreview extends React.Component {
-    render() {
-        return (
-            <div 
-                className="swatch-app__preview-box"
-                style={{backgroundColor: `rgb(${this.props.bgColor.r}, ${this.props.bgColor.g}, ${this.props.bgColor.b})`}}>
-                <div className="swatch-app__preview-box__controls">
-                    {this.props.children}
-                </div>
+const SwatchPreview = function(props) {
+    return (
+        <div 
+            className="swatch-app__preview-box"
+            style={{backgroundColor: `rgb(${props.bgColor.r}, ${props.bgColor.g}, ${props.bgColor.b})`}}>
+            <div className="swatch-app__preview-box__controls">
+                {props.children}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default SwatchPreview;
